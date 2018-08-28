6be771f50fc9ece1f006d0b76d5511cee85bba88
Bazhenov Eugen <wtf.czabe@gmail.com>
2018-08-28 17:59:18 +0300

    add eng local

--- tasks/render.js
+++ tasks/render.js
@@ -1,18 +1,29 @@
 const gulp = require('gulp');
 const data = require('gulp-data');
+const rename = require("gulp-rename");
 const nunjucksRender = require('gulp-nunjucks-render');
 const locals = {
-  ru: require('../src/pages/locals/ru.local.json'),
+  ru: {
+    prefix: '',
+    content: require('../src/pages/locals/ru.local.json')
+  },
+  en: {
+    prefix: 'en.',
+    content: require('../src/pages/locals/en.local.json')
+  },
 };
 
 module.exports = (browserSync) => gulp.task('render', function() {
   return Object.keys(locals).forEach(key => {
     return gulp.src('src/pages/*.+(html|njk)')
     .pipe(data(() => {
-      return locals[key];
+      return locals[key].content;
     }))
     .pipe(nunjucksRender({
       path: ['src/pages/templates'],
+    }))
+    .pipe(rename(function(path) {
+      path.basename = locals[key].prefix + path.basename;
     }))
     .pipe(gulp.dest('www/'))
     .pipe(browserSync.stream());
